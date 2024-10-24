320e0ba50c0d2d8971856084f7e2eb32bf37b9c2
tien.nhat <tiennhat306@gmail.com>
2024-10-24 20:42:56 +0700

    hotfix: fix enabled field's default vaue

--- src/main/resources/db/migration/V240917_02__create_users_table.sql
+++ src/main/resources/db/migration/V240917_02__create_users_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users
     username     VARCHAR(255),
     email        VARCHAR(255) NOT NULL,
     password     VARCHAR(255) NOT NULL,
-    enabled      BOOLEAN      NOT NULL DEFAULT TRUE,
+    enabled      BOOLEAN      NOT NULL DEFAULT FALSE,
     full_name    VARCHAR(555) NOT NULL,
     created_at   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
