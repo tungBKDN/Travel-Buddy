b71460effdeabed6e5ccd67dd6ebaafbe6ca9922
Tran Thanh Tung <102210138@sv1.dut.udn.vn>
2024-10-29 15:52:33 +0700

    feat: added PUT /api/admin/site-approval for censoring new site versions posts

--- src/main/java/com/travelbuddy/siteapproval/admin/SiteApprovalServiceImp.java
+++ src/main/java/com/travelbuddy/siteapproval/admin/SiteApprovalServiceImp.java
@@ -1,8 +1,13 @@
 package com.travelbuddy.siteapproval.admin;
 
+import com.travelbuddy.common.constants.ApprovalStatusEnum;
+import com.travelbuddy.persistence.domain.dto.siteapproval.UpdateSiteApprovalRqstDto;
 import com.travelbuddy.persistence.domain.entity.SiteApprovalEntity;
 import com.travelbuddy.persistence.repository.SiteApprovalRepository;
 import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Service
 public class SiteApprovalServiceImp implements SiteApprovalService {
@@ -21,6 +26,19 @@ public class SiteApprovalServiceImp implements SiteApprovalService {
         siteApprovalEntity = SiteApprovalEntity.builder().siteVersionId(siteVersionId).build();
 
         // Save the site approval
+        siteApprovalRepository.save(siteApprovalEntity);
+    }
+
+    @Override
+    public void updateSiteApproval(UpdateSiteApprovalRqstDto updateSiteApprovalRqstDto, Integer adminId) {
+        /*
+        * This function updates the site approval record when admin approves or rejects a site
+        * with adminId get from the JWTToken
+        */
+        SiteApprovalEntity siteApprovalEntity = siteApprovalRepository.findById(updateSiteApprovalRqstDto.getId()).orElseThrow(() -> new RuntimeException("Site approval not found"));
+        siteApprovalEntity.setStatus(ApprovalStatusEnum.valueOf(updateSiteApprovalRqstDto.getStatus()));
+        siteApprovalEntity.setAdminId(adminId);
+        siteApprovalEntity.setApprovedAt(Timestamp.valueOf(LocalDateTime.now()));
         siteApprovalRepository.save(siteApprovalEntity);
     }
 }
