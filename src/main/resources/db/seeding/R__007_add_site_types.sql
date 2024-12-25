0953f928b22bdd379bb013ee49764729499966a7
Tran Thanh Tung <102210138@sv1.dut.udn.vn>
2024-12-26 00:34:38 +0700

    fix: refactor seedings

--- src/main/resources/db/seeding/R__007_add_site_types.sql
+++ src/main/resources/db/seeding/R__007_add_site_types.sql
@@ -2,11 +2,61 @@ DO
 $$
     BEGIN
         IF NOT EXISTS (SELECT 1 FROM site_types) THEN
-            INSERT INTO site_types (id, type_name, dual_state)
-            VALUES  (1, 'Sân vận động', 'AMENITY'),
-                    (2, 'Bệnh viện', 'AMENITY'),
-                    (3, 'Khu vui chơi nước trong nhà', 'DUAL'),
-                    (4, 'Hiệu thuốc', 'AMENITY');
+            INSERT INTO site_types (type_name, dual_state)
+            VALUES ('Sân vận động', 'AMENITY'),       -- 1
+                   ('Bệnh viện', 'AMENITY'),
+                   ('Khu vui chơi nước trong nhà', 'DUAL'),
+                   ('Hiệu thuốc', 'AMENITY'),
+                   ('Nhà hàng', 'DUAL'),              -- 5
+                   ('Nhà hàng thức ăn nhanh', 'AMENITY'),
+                   ('Nhà hàng thuần chay', 'AMENITY'),
+                   ('Nhà hàng thịt nướng', 'AMENITY'),
+                   ('Nhà hàng Brunch', 'AMENITY'),
+                   ('Quán ăn nhỏ', 'DUAL'),           -- 10
+                   ('Nhà hàng thực phẩm lành mạnh', 'AMENITY'),
+                   ('Nhà hàng chay', 'AMENITY'),
+                   ('Nhà hàng cao cấp', 'DUAL'),
+                   ('Nhà hàng hải sản', 'AMENITY'),
+                   ('Nhà điều hành tour', 'AMENITY'), -- 15
+                   ('Nhà hàng gia đình', 'DUAL'),
+                   ('Nhà hàng bít tết', 'AMENITY'),
+                   ('Quán tapas', 'AMENITY'),
+                   ('Quán mì', 'AMENITY'),
+                   ('Nhà hàng cơm', 'DUAL'),          -- 20
+                   ('Quán cà phê', 'AMENITY'),
+                   ('Quầy xúc xích nóng', 'AMENITY'),
+                   ('Quán bánh mì', 'DUAL'),
+                   ('Nhà hàng buffet', 'DUAL'),
+                   ('Quầy đồ ăn nhẹ', 'AMENITY'),
+                   ('Điểm du lịch', 'ATTRACTION'),
+                   ('Khách sạn', 'AMENITY'),
+                   ('Khu nghỉ dưỡng khách sạn (Resort hotel)', 'ATTRACTION'),
+                   ('Quán bia thủ công', 'DUAL'),
+                   ('Hoa viên', 'ATTRACTION'),
+                   ('Nhà vườn cho thuê', 'DUAL'),
+                   ('Cửa hàng thực phẩm', 'AMENITY'),
+                   ('Nhà nghỉ dưỡng', 'DUAL'),
+                   ('Homestay', 'DUAL'),
+                   ('Pub', 'AMENITY'),
+                   ('Căn hộ nghỉ dưỡng', 'DUAL'),
+                   ('Khu cắm trại', 'ATTRACTION'),
+                   ('Phòng GYM', 'AMENITY'),
+                   ('Nhà thờ', 'DUAL'),
+                   ('Villa', 'DUAL'),
+                   ('Nông trại', 'ATTRACTION'),
+                   ('Vườn', 'ATTRACTION'),
+                   ('Vườn cây', 'ATTRACTION'),
+                   ('Công viên', 'ATTRACTION'),
+                   ('Nông trại cắm trại', 'ATTRACTION'),
+                   ('Khu đi bộ', 'AMENITY'),
+                   ('Cơ quan du lịch lặn', 'AMENITY'),
+                   ('Công viên sinh thái', 'ATTRACTION'),
+                   ('Công viên tưởng niệm', 'ATTRACTION'),
+                   ('Rừng quốc gia', 'ATTRACTION'),
+                   ('Công viên đồng quê', 'ATTRACTION'),
+                   ('Vườn bách thảo', 'ATTRACTION'),
+                   ('Quán ăn', 'DUAL'); -- 53
+
         END IF;
     END;
 $$;
--- src/main/resources/db/seeding/R__009_add_service_groups.sql
+++ src/main/resources/db/seeding/R__009_add_service_groups.sql
@@ -5,7 +5,11 @@ $$
             INSERT INTO service_groups (id, service_group_name)
             VALUES (1, 'Tiện nghi'),
                    (2, 'Tiện nghi trong phòng'),
-                   (3, 'Loại phòng');
+                   (3, 'Loại phòng'),
+                   (4, 'Dịch vụ ăn uống'),
+                   (5, 'Thanh toán'),
+                   (6, 'Vệ sinh thể dục'),
+                   (7, 'Xe cộ');
         END IF;
     END;
 $$;
